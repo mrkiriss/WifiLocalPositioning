3b55df5370107610055efb583d55f8eae9839f29
kirill <kvoskrebentsev@mail.ru>
2021-05-07 02:04:42 +0300

    Implemented a change in the number of scans in the settings, processed the case of the first launch of the application

---
+++ app/src/main/java/com/mrkiriss/wifilocalpositioning/data/models/preference/DefaultSettings.java
@@ -0,0 +1,6 @@
+package com.mrkiriss.wifilocalpositioning.data.models.preference;
+
+public class DefaultSettings {
+    public static String defaultScanInterval="5"; // in seconds
+    public static String defaultVariousValue = "1";
+}
--- app/src/main/java/com/mrkiriss/wifilocalpositioning/mvvm/view/SettingsFragment.java
+++ app/src/main/java/com/mrkiriss/wifilocalpositioning/mvvm/view/SettingsFragment.java
@@ -12,6 +12,7 @@ import androidx.preference.PreferenceFragmentCompat;
 import androidx.preference.PreferenceManager;
 
 import com.mrkiriss.wifilocalpositioning.R;
+import com.mrkiriss.wifilocalpositioning.data.models.preference.DefaultSettings;
 import com.mrkiriss.wifilocalpositioning.data.models.preference.Settings;
 import com.mrkiriss.wifilocalpositioning.data.sources.db.SettingsDao;
 
@@ -24,8 +25,8 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     private PreferenceManager preferenceManager;
     private SharedPreferences sharedPreferences;
 
-    private final String defaultScanInterval="5"; // in seconds
-    private final String defaultVarious = "Минимально (1 ск.)";
+    private final String defaultScanInterval= DefaultSettings.defaultScanInterval; // in seconds
+    private final String defaultVarious = DefaultSettings.defaultVariousValue;
 
     private final String[] keys = new String[]{"scanInterval", "variousOfNumberScans"};
 
@@ -36,15 +37,24 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.root_preferences);
 
+        // find views
         scanIntervalEditText=findPreference(keys[0]);
-        variousNumbersList = findPreference("variousOfNumberScans");
+        variousNumbersList = findPreference(keys[1]);
 
+        // create preference objects
         preferenceManager=getPreferenceManager();
         sharedPreferences=preferenceManager.getSharedPreferences();
 
+        // register preference change listener
         sharedPreferences.registerOnSharedPreferenceChangeListener(this::processEditPreferences);
 
+        // set summaries
         scanIntervalEditText.setSummary(preferenceManager.getSharedPreferences().getString(keys[0], defaultScanInterval));
+        variousNumbersList.setSummary(preferenceManager.getSharedPreferences().getString(keys[1], defaultVarious));
+
+        // set selected values
+        if (scanIntervalEditText.getText()==null || scanIntervalEditText.getText().isEmpty()) scanIntervalEditText.setText(defaultScanInterval);
+        if (variousNumbersList.getValue()==null || variousNumbersList.getValue().isEmpty()) variousNumbersList.setValue(defaultVarious);
     }
 
     private void processEditPreferences(SharedPreferences sharedPreferences, String key){
@@ -61,14 +71,13 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     }
     private void processScanIntervalKey(SharedPreferences sharedPreferences, String key){
         try {
-            long input =  (long) Float.parseFloat(sharedPreferences.getString(key, defaultScanInterval));
-            if (input<0){
+            String input = sharedPreferences.getString(key, defaultScanInterval);
+            if (!input.matches("\\d+")){
                 throw new Exception();
             }
             scanIntervalEditText.setSummary(String.valueOf(input));
             Log.i("SettingsFragments", "new value for scanInterval="+input);
         }catch (Exception e){
-            //sharedPreferences.edit().putString(key, defaultScanInterval).apply();
             scanIntervalEditText.setText(scanIntervalEditText.getSummary().toString());
             showToastContent("Неккоректные данные");
             e.printStackTrace();
@@ -77,9 +86,13 @@ public class SettingsFragment extends PreferenceFragmentCompat {
     private void processVariousOfNumbersScans(SharedPreferences sharedPreferences, String key){
         try {
             String input =  sharedPreferences.getString(key, defaultVarious);
+            if (input.isEmpty()) throw new Exception();
+            variousNumbersList.setSummary(sharedPreferences.getString(key, defaultVarious));
             Log.i("SettingsFragments", "new value for variousOfNumberScans="+input);
         }catch (Exception e){
             sharedPreferences.edit().putString(key, defaultVarious).apply();
+            variousNumbersList.setValue(defaultVarious);
+            variousNumbersList.setSummary(defaultVarious);
             showToastContent("Неккоректные данные");
             e.printStackTrace();
         }
